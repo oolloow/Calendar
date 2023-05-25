78f6defb9940f983abaf70da9f7103cf460a8c08
Alex Zaytsev <az@oolloow.com>
2023-05-25 14:28:18 +1000

    And end interaction otherwise.

--- CalendarLib/MGCDayPlannerView.m
+++ CalendarLib/MGCDayPlannerView.m
@@ -1229,7 +1229,9 @@ static const CGFloat kMaxHourSlotHeight = 150.;
 			}
 			else if (date && self.movingEventDate && [self.dataSource respondsToSelector:@selector(dayPlannerView:moveEventOfType:atIndex:date:toType:date:)]) {
 				[self.dataSource dayPlannerView:self moveEventOfType:self.movingEventType atIndex:self.movingEventIndex date:self.movingEventDate toType:self.interactiveCellType date:date];
-			}
+            } else {
+                [self endInteraction];
+            }
 		}
 		else  // new event
 		{
